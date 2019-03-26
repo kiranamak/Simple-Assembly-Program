c3d3e0c01ae15a6231bab15842f4259a2bb60ca1
Kiran Mak <kmak@macbook-pro.fios-router.home>
2019-03-26 19:50:13 -0400

    Initial Commit

---
+++ Simple Assembly Program/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Simple Assembly Program
+//
+//  Created by Kiran Mak on 3/26/19.
+//  Copyright © 2019 Kiran Mak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
